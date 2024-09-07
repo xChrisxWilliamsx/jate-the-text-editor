bf89cda47e8562be0c88df8fec44bc4096a928a5
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-07 13:25:36 -0700

    Adds config to webpack.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,11 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+//--
+
 // TODO: Add CSS loaders and babel to webpack.
+//--
+
 
 module.exports = () => {
   return {
@@ -18,13 +22,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'J.A.T.E. The Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Your premier, on the go, text editor that works in all environments!',
+        background_color: '#272822',
+        theme_color: '#43315F',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }), 
+
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
-    },
+     },
   };
 };
