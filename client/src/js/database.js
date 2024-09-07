181f06fee7d0c39c7afeed8a6991adb98ae4ff23
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-07 16:47:03 -0700

    Adds get and put functions into db js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const idb = await openDB('jate', 1);
+    const ta = idb.transaction('jate', 'readwrite');
+    const store = ta.objectStore('jate');
+    const request = store.add({ value: content });
+    const result = await request;
+    console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('putDb not implemented:', error);
+  };
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const idb = await openDB('jate', 1);
+    const ta = idb.transaction('jate', 'readonly');
+    const store = ta.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('getDb not implemented:', error);
+  };
+};
 
 initdb();
